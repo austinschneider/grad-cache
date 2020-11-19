42cf859f44431ff35acaac71d20c8132bf1f146a
Austin Schneider <hogenshpogen@gmail.com>
2020-11-19 16:18:26 -0500

    Use nodes to do the forward evaluation

--- gradcache/node.py
+++ gradcache/node.py
@@ -1,38 +1,72 @@
 import numpy as np
 import types
 from functools import wraps
-import gradcache.autodiff as ad
+#import gradcache.autodiff as ad
+import autodiff as ad
+from operators import operators as ops
 
 
-def register_op(cls, method, token, n, rev):
+def get_value(x):
+    if isinstance(x, Node):
+        return x.value
+    else:
+        return x
+    return x.value if isinstance(x, Node) else x
+
+def build_op(token, n, rev):
     if n == 2:
         op = str(token)
         if rev:
             def inner(self, other):
-                otype = type(other)
-                if otype is not Node:
-                    other = Constant(other)
-                return Node(op, [other, self])
+                if self.evaluate:
+                    sval = get_value(self)
+                    oval = get_value(other)
+                    res = ops[op].eval(oval, sval)
+                    return Node(op, [], value=res)
+                else:
+                    if not isinstance(other, Node):
+                        other = Constant(other)
+                    return Node(op, [other, self])
         else:
             def inner(self, other):
-                otype = type(other)
-                if otype is not Node:
-                    other = Constant(other)
-                return Node(op, [self, other])
+                if self.evaluate:
+                    sval = get_value(self)
+                    oval = get_value(other)
+                    res = ops[op].eval(sval, oval)
+                    return Node(op, [], value=res)
+                else:
+                    if not isinstance(otype, Node):
+                        other = Constant(other)
+                    return Node(op, [self, other])
     elif n == 1:
         op = str(token)
         def inner(self):
-            return Node(op, [self])
+            if self.evaluate:
+                sval = get_value(self)
+                res = ops[op].eval(sval)
+                return Node(op, [], value=res)
+            else:
+                return Node(op, [self])
+    return inner
 
-    setattr(cls, method, inner)
+
+def register_op(cls, method, token, n, rev):
+    op = build_op(token, n, rev)
+    setattr(cls, method, op)
 
 
 class Node:
     """A node in a computation graph."""
-    def __init__(self, op, children):
+    def __init__(self, op, children, value=None):
         self.children = children
         self.op = op
         self.name = None
+        if value is None:
+            self.evaluate = False
+            self.value = None
+        else:
+            self.evaluate = True
+            self.value = value
     @classmethod
     def register_op(cls, *args):
         register_op(cls, *args)
@@ -72,78 +106,57 @@ operators = [
         ("__pow__", "pow", 2, False),
         ("__rpow__", "pow", 2, True),
         ("__invert__", "inv", 1, False),
+        ("log", "log", 1, False),
+        ("log10", "log10", 1, False),
+        ("log2", "log2", 1, False),
+        ("sqrt", "sqrt", 1, False),
+        ("lgamma", "lgamma", 1, False),
+        ("log1p", "log1p", 1, False),
         ]
 
 for op in operators:
     Node.register_op(*op)
 
-def log(node):
-    op = 'log'
-    return Node(op, [node])
+def set_op(obj, method, token, n, rev):
+    op = build_op(token, n, rev)
+    obj[method] = op
 
-def log10(node):
-    op = 'log10'
-    return Node(op, [node])
+methods = [
+        "log",
+        "log10",
+        "log2",
+        "sqrt",
+        "lgamma",
+        "log1p",
+        ]
 
-def log2(node):
-    op = 'log2'
-    return Node(op, [node])
-
-def sqrt(node):
-    op = 'sqrt'
-    return Node(op, [node])
-
-def lgamma(node):
-    op = 'lgamma'
-    return Node(op, [node])
-
-def log1p(node):
-    op = 'log1p'
-    return Node(op, [node])
+for method in methods:
+    set_op(globals(), method, method, 1, False)
 
 
-"""
-    def __add__(self, other):
-        op = 'plus'
-        return Node(op, [self, other])
+if __name__ == "__main__":
 
-    def __radd__(self, other):
-        op = 'plus'
-        return Node(op, [other, self])
+    def f(a, b, c, d):
+        y = a + b
+        z = c + d
+        r = (y**2) * z
+        return r
 
-    def __sub__(self, other):
-        op = 'minus'
-        return Node(op, [self, other])
+    from parameter_wrapper import parameter_wrapper, sift_parameters
 
-    def __rsub__(self, other):
-        op = 'minus'
-        return Node(op, [other, self])
+    a = parameter_wrapper('a', 1, grads=['g'], grad_values=[1])
+    b = parameter_wrapper('b', 1, grads=['g'], grad_values=[1])
+    c = parameter_wrapper('c', 1, grads=['h'], grad_values=[1])
+    d = parameter_wrapper('d', 1, grads=['h'], grad_values=[1])
 
-    def __mul__(self, other):
-        op = 'mul'
-        return Node(op, [self, other])
+    na = Node("a", [], value=a)
+    nb = Node("b", [], value=b)
+    nc = Node("c", [], value=c)
+    nd = Node("d", [], value=d)
 
-    def __rmul__(self, other):
-        op = 'mul'
-        return Node(op, [other, self])
+    res = f(na, nb, nc, nd)
+    print(res.value)
+    print(res.value.value)
+    print(res.value.grads)
+    print(res.value.grad_values)
 
-    def __div__(self, other):
-        op = 'div'
-        return Node(op, [self, other])
-
-    def __rdiv__(self, other):
-        op = 'div'
-        return Node(op, [other, self])
-
-    def __pow__(self, other):
-        op = 'pow'
-        return Node(op, [self, other])
-
-    def __rpow__(self, other):
-        op = 'pow'
-        return Node(op, [other, self])
-
-    def __invert__(self):
-        op = 'inv'
-        return Node(op, [self])
-"""
